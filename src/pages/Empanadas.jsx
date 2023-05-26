c29d70b1a67dba7a8eb04b30d9e4c6441254da79
Matias Piovesan <piovesanmatias@gmail.com>
2023-05-26 13:12:06 -0300

    Actualizacion de precios

--- src/pages/Empanadas.jsx
+++ src/pages/Empanadas.jsx
@@ -6,19 +6,19 @@ const Empanadas = () =>{
     return(
         <div>
             <Navbar/>
-            <Cartas nombre='Empanadas de carne suave' descripcion='' precio='$220' />
-            <Cartas nombre='Empanadas de carne picante' descripcion='' precio='$220' />
-            <Cartas nombre='Empanadas de pollo suave' descripcion='' precio='$220' />
-            <Cartas nombre='Empanadas de pollo picante' descripcion='' precio='$220' />
-            <Cartas nombre='Empanadas de Jamon y queso' descripcion='' precio='$220' />
-            <Cartas nombre='Empanadas de choclo' descripcion='Queso y choclo' precio='$220' />
-            <Cartas nombre='Sfijas' descripcion='Empanadas Arabes' precio='$220' />
-            <Cartas nombre='Cantimpalo' descripcion='Cantimpalo, queso y aceitunas negras' precio='$220' />
-            <Cartas nombre='Roquefort' descripcion='Roquefort, apio, muzarella' precio='$220' />
-            <Cartas nombre='Queso' descripcion='Queso, cebolla de verdeo y huevo' precio='$220' />
-            <Cartas nombre='Queso aceituna y tomate' descripcion='' precio='$220' />
-            <Cartas nombre='Ternera' descripcion='Queso y tomate' precio='$220' />
-            <Cartas nombre='Docena de empanadas' descripcion='12 unidades' precio='$2640' />
+            <Cartas nombre='Empanadas de carne suave' descripcion='' precio='$250' />
+            <Cartas nombre='Empanadas de carne picante' descripcion='' precio='$250' />
+            <Cartas nombre='Empanadas de pollo suave' descripcion='' precio='$250' />
+            <Cartas nombre='Empanadas de pollo picante' descripcion='' precio='$250' />
+            <Cartas nombre='Empanadas de Jamon y queso' descripcion='' precio='$250' />
+            <Cartas nombre='Empanadas de choclo' descripcion='Queso y choclo' precio='$250' />
+            <Cartas nombre='Sfijas' descripcion='Empanadas Arabes' precio='$250' />
+            <Cartas nombre='Cantimpalo' descripcion='Cantimpalo, queso y aceitunas negras' precio='$250' />
+            <Cartas nombre='Roquefort' descripcion='Roquefort, apio, muzarella' precio='$250' />
+            <Cartas nombre='Queso' descripcion='Queso, cebolla de verdeo y huevo' precio='$250' />
+            <Cartas nombre='Queso aceituna y tomate' descripcion='' precio='$250' />
+            <Cartas nombre='Ternera' descripcion='Queso y tomate' precio='$250' />
+            <Cartas nombre='Docena de empanadas' descripcion='12 unidades' precio='$3000' />
      
         </div>
     )
--- src/pages/Sanguheria.jsx
+++ src/pages/Sanguheria.jsx
@@ -6,21 +6,21 @@ const Sangucheria = () =>{
     return(
         <div>
         <Navbar/>
-        <Cartas nombre='Sanguche de milanesa' descripcion='' precio='$1100' />
-        <Cartas nombre='Sanguche de milanesa Cordero' descripcion='Cheddar y bacon' precio='$1300' />
-        <Cartas nombre='Milanesa al plato' descripcion='' precio='$1300' />
-        <Cartas nombre='Sanguche de lomito' descripcion='' precio='$1200' />
-        <Cartas nombre='Sanguche de lomito Cordero' descripcion='Cheddar y bacon' precio='$1400' />
-        <Cartas nombre='Lomito al plato' descripcion='Con ensalada' precio='$1400' />
-        <Cartas nombre='Sanguche radical' descripcion='Lomo grueso' precio='$1400' />
-        <Cartas nombre='Sanguche radical Cordero' descripcion='Lomo grueso, cheddar y bacon' precio='$1600' />
-        <Cartas nombre='Radical al plato' descripcion='Con ensalada' precio='$1600' />
-        <Cartas nombre='Hamburguesa' descripcion='' precio='$900' />
-        <Cartas nombre='Hamburguesa Cordero' descripcion='Cheddar y bacon' precio='$1000' />
-        <Cartas nombre='Hamburguesa al plato' descripcion='Con ensalada' precio='$900' />
-        <Cartas nombre='Tostado de jamon y queso' descripcion='' precio='$850' />
-        <Cartas nombre='Tostado de ternera' descripcion='' precio='$800' />
-        <Cartas nombre='Vegetariano' descripcion='Huevo y queso' precio='$650' />
+        <Cartas nombre='Sanguche de milanesa' descripcion='' precio='$1200' />
+        <Cartas nombre='Sanguche de milanesa Cordero' descripcion='Cheddar y bacon' precio='$1400' />
+        <Cartas nombre='Milanesa al plato' descripcion='' precio='$1400' />
+        <Cartas nombre='Sanguche de lomito' descripcion='' precio='$1300' />
+        <Cartas nombre='Sanguche de lomito Cordero' descripcion='Cheddar y bacon' precio='$1500' />
+        <Cartas nombre='Lomito al plato' descripcion='Con ensalada' precio='$1500' />
+        <Cartas nombre='Sanguche radical' descripcion='Lomo grueso' precio='$1500' />
+        <Cartas nombre='Sanguche radical Cordero' descripcion='Lomo grueso, cheddar y bacon' precio='$1700' />
+        <Cartas nombre='Radical al plato' descripcion='Con ensalada' precio='$1700' />
+        <Cartas nombre='Hamburguesa' descripcion='' precio='$1000' />
+        <Cartas nombre='Hamburguesa Cordero' descripcion='Cheddar y bacon' precio='$1100' />
+        <Cartas nombre='Hamburguesa al plato' descripcion='Con ensalada' precio='$1000' />
+        <Cartas nombre='Tostado de jamon y queso' descripcion='' precio='$950' />
+        <Cartas nombre='Tostado de ternera' descripcion='' precio='$900' />
+        <Cartas nombre='Vegetariano' descripcion='Huevo y queso' precio='$750' />
         </div>
         
     )
