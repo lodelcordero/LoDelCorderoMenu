f0c5971d51c6d7b66915bbc0f777606d816932b9
Matias Piovesan <piovesanmatias@gmail.com>
2023-08-19 18:57:38 -0300

    Cambios en los precios

--- src/pages/Sanguheria.jsx
+++ src/pages/Sanguheria.jsx
@@ -6,15 +6,15 @@ const Sangucheria = () =>{
     return(
         <div>
         <Navbar/>
-        <Cartas nombre='Sanguche de milanesa' descripcion='' precio='$1600' />
-        <Cartas nombre='Sanguche de milanesa especial' descripcion='Queso, jamon y huevo' precio='$2200' />
-        <Cartas nombre='Sanguche de milanesa Cordero' descripcion='Cheddar y bacon' precio='$1800' />
+        <Cartas nombre='Sanguche de milanesa' descripcion='' precio='$1800' />
+        <Cartas nombre='Sanguche de milanesa especial' descripcion='Queso, jamon y huevo' precio='$2400' />
+        <Cartas nombre='Sanguche de milanesa Cordero' descripcion='Cheddar y bacon' precio='$2400' />
         <Cartas nombre='Milanesa al plato' descripcion='' precio='$1700' />
-        <Cartas nombre='Sanguche de lomito' descripcion='' precio='$1750' />
-        <Cartas nombre='Sanguche de lomito Cordero' descripcion='Cheddar y bacon' precio='$2000' />
+        <Cartas nombre='Sanguche de lomito' descripcion='' precio='$2000' />
+        <Cartas nombre='Sanguche de lomito Cordero' descripcion='Cheddar y bacon' precio='$2600' />
         <Cartas nombre='Lomito al plato' descripcion='Con ensalada' precio='$1900' />
-        <Cartas nombre='Sanguche radical' descripcion='Lomo grueso' precio='$2000' />
-        <Cartas nombre='Sanguche radical Cordero' descripcion='Lomo grueso, cheddar y bacon' precio='$2000' />
+        <Cartas nombre='Sanguche radical' descripcion='Lomo grueso' precio='$2300' />
+        <Cartas nombre='Sanguche radical Cordero' descripcion='Lomo grueso, cheddar y bacon' precio='$2900' />
         <Cartas nombre='Radical al plato' descripcion='Con ensalada' precio='$2100' />
         <Cartas nombre='Hamburguesa' descripcion='' precio='$1350' />
         <Cartas nombre='Hamburguesa Cordero' descripcion='Cheddar y bacon' precio='$1600' />
