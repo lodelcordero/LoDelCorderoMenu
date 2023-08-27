df75b9a48dd579559d69ac23fe1a5bcdba5203c7
Matias Piovesan <piovesanmatias@gmail.com>
2023-08-26 22:43:55 -0300

    Cambio precio de empanadas

--- src/pages/Empanadas.jsx
+++ src/pages/Empanadas.jsx
@@ -6,19 +6,19 @@ const Empanadas = () =>{
     return(
         <div>
             <Navbar/>
-            <Cartas nombre='Empanadas de carne suave' descripcion='' precio='$300' />
-            <Cartas nombre='Empanadas de carne picante' descripcion='' precio='$300' />
-            <Cartas nombre='Empanadas de pollo suave' descripcion='' precio='$300' />
-            <Cartas nombre='Empanadas de pollo picante' descripcion='' precio='$300' />
-            <Cartas nombre='Empanadas de Jamon y queso' descripcion='' precio='$300' />
-            <Cartas nombre='Empanadas de choclo' descripcion='Queso y choclo' precio='$300' />
-            <Cartas nombre='Sfijas' descripcion='Empanadas Arabes' precio='$300' />
-            <Cartas nombre='Cantimpalo' descripcion='Cantimpalo, queso y aceitunas negras' precio='$300' />
-            <Cartas nombre='Roquefort' descripcion='Roquefort, apio, muzarella' precio='$300' />
-            <Cartas nombre='Queso' descripcion='Queso, cebolla de verdeo y huevo' precio='$300' />
-            <Cartas nombre='Queso aceituna y tomate' descripcion='' precio='$300' />
-            <Cartas nombre='Ternera' descripcion='Queso y tomate' precio='$300' />
-            <Cartas nombre='Docena de empanadas' descripcion='12 unidades' precio='$3600' />
+            <Cartas nombre='Empanadas de carne suave' descripcion='' precio='$350' />
+            <Cartas nombre='Empanadas de carne picante' descripcion='' precio='$350' />
+            <Cartas nombre='Empanadas de pollo suave' descripcion='' precio='$350' />
+            <Cartas nombre='Empanadas de pollo picante' descripcion='' precio='$350' />
+            <Cartas nombre='Empanadas de Jamon y queso' descripcion='' precio='$350' />
+            <Cartas nombre='Empanadas de choclo' descripcion='Queso y choclo' precio='$350' />
+            <Cartas nombre='Sfijas' descripcion='Empanadas Arabes' precio='$350' />
+            <Cartas nombre='Cantimpalo' descripcion='Cantimpalo, queso y aceitunas negras' precio='$350' />
+            <Cartas nombre='Roquefort' descripcion='Roquefort, apio, muzarella' precio='$350' />
+            <Cartas nombre='Queso' descripcion='Queso, cebolla de verdeo y huevo' precio='$350' />
+            <Cartas nombre='Queso aceituna y tomate' descripcion='' precio='$350' />
+            <Cartas nombre='Ternera' descripcion='Queso y tomate' precio='$350' />
+            <Cartas nombre='Docena de empanadas' descripcion='12 unidades' precio='$4200' />
      
         </div>
     )
